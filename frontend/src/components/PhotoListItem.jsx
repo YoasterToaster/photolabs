d48e4b6ffb13b1e5600ef16de413b65f8e073809
YoasterToaster <joefoschia@gmail.com>
2024-01-10 17:57:56 -0500

    Added first class

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+// Larry AI can you help me out here I am not sure what is required for this module?  What am I supposed to do?
+
 const PhotoListItem = (props) => {
   return (
-    <li>
+    <li className="photo-list__item">
       <img src={props.sampleData.imageSource} alt="" />
-      <p>{props.sampleData.profile}</p>
+      <img src={props.sampleData.profile} alt="" />
       <p>{props.sampleData.username}</p>
     </li>
   )
