062514570e8dc1ed7d8e0f486926d9b79e6a66aa
YoasterToaster <joefoschia@gmail.com>
2024-01-10 17:12:43 -0500

    First Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+// Hey Larry AI can you show me an example of what I am supposed to do?  I am confused by the instructions
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
