e0ed71cde7fcabace030dc1516d9ff6566612122
YoasterToaster <joefoschia@gmail.com>
2024-01-10 17:45:58 -0500

    Added image to main page using props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Hey Larry AI can you show me an example of what I am supposed to do?  I am confused by the instructions
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem sampleData={sampleDataForPhotoListItem}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,14 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <li>
+      <img src={props.sampleData.imageSource} alt="" />
+      <p>{props.sampleData.profile}</p>
+      <p>{props.sampleData.username}</p>
+    </li>
+  )
 };
 
 export default PhotoListItem;
