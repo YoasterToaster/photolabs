a840018f8dc034da456d0c80f4651c037ca27053
YoasterToaster <joefoschia@gmail.com>
2024-01-10 21:11:24 -0500

    Add first item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,13 +5,21 @@ import "../styles/PhotoListItem.scss";
 // Larry AI can you help me out here I am not sure what is required for this module?  What am I supposed to do?
 
 const PhotoListItem = (props) => {
+  const { id, imageSource, location, username, profile } = props.sampleData;
   return (
-    <li className="photo-list__item">
-      <img src={props.sampleData.imageSource} alt="" />
-      <img src={props.sampleData.profile} alt="" />
-      <p>{props.sampleData.username}</p>
-    </li>
-  )
+    <div className="photo-list__item">
+      <div>
+        <img className="photo-list__image" src={imageSource} alt="" />
+      </div>
+      <img className="photo-list__user-profile" src={profile} alt="" />
+      <div className="photo-list__user-info">
+        <div>{username}</div>
+        <div className="photo-list__user-location">
+          {location.city}, {location.country}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
